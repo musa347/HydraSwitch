e27421fc0654ab98f2665eb50ced2814156c7468
Ibrahim Musa <imusa@imusa-sg1-2.local>
2025-11-15 22:15:39 +0100

    project setup

---
+++ mock-banks/src/main/java/com/hydraswitch/mock_banks/MockBanksApplication.java
@@ -0,0 +1,13 @@
+package com.hydraswitch.mock_banks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockBanksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockBanksApplication.class, args);
+	}
+
+}
---
+++ workers/src/main/java/com/hydraswitch/workers/WorkersApplication.java
@@ -0,0 +1,13 @@
+package com.hydraswitch.workers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkersApplication.class, args);
+	}
+
+}
